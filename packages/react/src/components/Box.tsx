7adfba7ba1f86ba87d23402a4cd6e36493d7f8ff
Enos Domingues <mail@enos.dev>
2024-11-05 12:09:24 +0100

    fix: correct React package index and create Box component story
    - Fix: Corrected the index file of the React package
    
    - Feat: Added Storybook story for Box component

---
+++ packages/react/src/components/Box.tsx
@@ -0,0 +1,12 @@
+import { styled } from '../styles'
+import type { ComponentProps } from 'react'
+
+export const Box = styled('div', {
+  padding: '$4',
+  borderRadius: '$md',
+  backgroundColor: '$gray800',
+  border: '1px solid $gray600',
+})
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface BoxProps extends ComponentProps<typeof Box> {}
